20cba3c72b857cc4391c8635901d5993ced41fbe
John <averagehero2@gmail.com>
2024-03-19 22:03:37 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from util.api.AnimeList import AnimeList
 from tinydb import TinyDB, Query
-
+### This will only work for one person because I don't care enough to allow for dynamic naming for the db ###
 db = TinyDB('./database.json')
 t = AnimeList()
 table = db.table('EldListings')
 s = Query()
 
 listings = t.getAnimes()
+runtimeChanges = 0
 for each in listings:
     search = table.search(s['showId'] == each.show.showId)
     if search == []:
@@ -22,3 +23,15 @@ for each in listings:
             oldScores = oldRecord['oldScores']
             oldScores.append(oldRecord['rating'])
             table.update({'rating': each.rankDetails.score, 'timesChanged': timesChanged+1, 'oldScores': oldScores}, s['showId'] == each.show.showId)
+            runtimeChanges += 1
+
+changesTable = db.table('TotalChanges')
+if changesTable.search(s['name'] == 'Eld') == []:
+    print('e not found')
+    changesTable.insert({'name': 'Eld', 'count': 0})
+else:
+    if runtimeChanges != 0:
+        res = changesTable.search(s['name'] =='Eld')[0]
+        oldValue = res
+        changesTable.update({'count': res['count']+runtimeChanges}, s['name'] =='Eld')
+print(f"Total changes: {runtimeChanges}")
